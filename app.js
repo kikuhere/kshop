e3ae37372feeb291af672e01bbc6d65a570b6be6
kikuhere <ukkumar@kiran.com>
2021-12-28 22:06:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const pug = require("pug");
+const path = require("path");
+
+app.set("view engine", "pug");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+const products = [];
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+app.get("/store", (req, res, next) => {
+  res.render("store", { allProducts: products });
+});
+
+app.post("/addproduct", (req, res, next) => {
+  products.unshift(req.body.title);
+  res.redirect("/store");
+});
+
+app.listen(3000, () => {
+  console.log("server started at 3000");
+});
